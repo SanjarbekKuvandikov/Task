aa912e91a6eb6b23e8cf92d264fe63ddc82dd5a2
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-07-21 15:50:41 +0500

    created project and added reactive implementations for gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-r2dbc
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:3.5.3")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+	// https://mvnrepository.com/artifact/org.postgresql/r2dbc-postgresql
+	implementation("org.postgresql:r2dbc-postgresql:1.0.7.RELEASE")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
