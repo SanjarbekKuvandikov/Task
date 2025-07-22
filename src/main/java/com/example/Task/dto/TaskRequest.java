f9c1db80782803bb98202811281b2caa53ac2671
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-07-22 10:55:12 +0500

    TaskRequest dto class created

---
+++ src/main/java/com/example/Task/dto/TaskRequest.java
@@ -0,0 +1,14 @@
+package com.example.Task.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskRequest {
+    @NotBlank(message = "Title cannot be empty")
+    private String title;
+    @NotBlank(message = "Description cannot be empty")
+    private String description;
+}
