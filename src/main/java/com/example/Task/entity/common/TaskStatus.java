1c2efd8605f983dd437a346d8bc038290ae6e1d3
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-07-22 10:34:39 +0500

    TaskStatus enum class created to save status

---
+++ src/main/java/com/example/Task/entity/common/TaskStatus.java
@@ -0,0 +1,11 @@
+package com.example.Task.entity.common;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    READY_FOR_REVIEW,
+    IN_REVIEW,
+    READY_FOR_TEST,
+    IN_TEST,
+    DONE
+}
